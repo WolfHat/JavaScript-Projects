fd828c741126350d0793949ecba4c64a2264c869
Maya <mayadelzer@gmail.com>
2022-08-08 01:42:34 -0700

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,7 +1,15 @@
+//Initiates two variables (a and b)
 var a = "beep ", b = "boop";
+//colours a to be blue
 var a = a.fontcolor("blue");
+//colours b to be red
 var b = b.fontcolor("red");
+//assigns a variable with an escaped apostrphe
 var c = "hey y\'all";
+//an expression
 var funnyNumber = 4 * 105;
+//a variable concatenating other variables
+importantMessage = a + b + funnyNumber;
+//showing the results on screen
 window.alert(c);
-document.write(a + b);
+document.write(importantMessage);
