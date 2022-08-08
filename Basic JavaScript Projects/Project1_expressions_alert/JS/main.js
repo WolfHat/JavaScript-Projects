d8879e7b256fed0b7d6cacef9814e9e7b33ff629
Maya <mayadelzer@gmail.com>
2022-08-08 01:38:03 -0700

    Submission1

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,7 @@
-document.write("Hello, World!");
+var a = "beep ", b = "boop";
+var a = a.fontcolor("blue");
+var b = b.fontcolor("red");
+var c = "hey y\'all";
+var funnyNumber = 4 * 105;
+window.alert(c);
+document.write(a + b);
